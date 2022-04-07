0b4dde69cd09a1051c6b34f7eff502820f5067ab
diegolarraz <diegolarraz1@gmail.com>
2022-04-07 09:04:07 +0100

    fixing img path

--- src/utils/assets.js
+++ src/utils/assets.js
@@ -1,14 +1,14 @@
 export const images = {
     WAVE_BG: {
-        url: "/images/wave.svg",
+        url: "./images/wave.svg",
         alt: "Wave Background"
     },
     ADD: {
-        url: "/images/add.png",
+        url: "./images/add.png",
         alt: "add"
     },
     CHECK: {
-        url: "/images/check.png",
+        url: "./images/check.png",
         alt: "check"
     }
 }

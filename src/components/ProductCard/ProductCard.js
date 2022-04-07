bebd2f571e7b3a6b20f040892e7f62f062787dc4
diegolarraz <diegolarraz1@gmail.com>
2022-04-07 02:36:54 +0100

    styled product card

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types';
 import classes from './ProductCard.module.css'
 import {useList} from '../../hooks/useList'
+import { images } from '../../utils/assets';
 const ProductCard = (props) => {
+    const { ADD, CHECK } = images;
+    const { name, image, price } = props.hit;
 
-    const { name, image, price } = props.hit
-    const { addToList } = useList()
+    const { addToList } = useList();
+    const [isSaved, setIsSaved] = useState(false)
+
+    // only trigger once to save to the basket
+    const handleClick =()=> {
+      if (!isSaved) {
+        addToList(props.hit)
+        setIsSaved(true)
+      }
+    }
     return (
         <div className={classes.CardContent}>
           <img src={image} alt={name} />
           <div className={classes.CardText}>
             <h5>{name.substring(0, 60)}</h5>
-
             <div className={classes.Details}>
               <p>Price: ${price}</p>
-              <button onClick={() => addToList(props.hit)} className={`${classes.Hidden}`}>
-                Add
-              </button>
-
+              <div 
+                onClick={handleClick} 
+                className={isSaved ? classes.GreenTick : classes.AddIcon}
+              >
+                <img src={isSaved ? CHECK.url : ADD.url} alt={isSaved ? CHECK.alt : ADD.alt} />
+              </div>
             </div>
           </div>
         </div>
--- src/utils/assets.js
+++ src/utils/assets.js
@@ -3,4 +3,12 @@ export const images = {
         url: "/images/wave.svg",
         alt: "Wave Background"
     },
+    ADD: {
+        url: "/images/add.png",
+        alt: "add"
+    },
+    CHECK: {
+        url: "/images/check.png",
+        alt: "check"
+    }
 }
